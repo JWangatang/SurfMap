42591f3b37bd58c829b16f411b78116279ef04b9
Patrick Woo-Sam <pwoosam5@icloud.com>
2017-01-21 15:58:51 -0800

    Updated SurfData class
    SurfData class constructor will gather surf_max, surf_min and
    coordinates for all spots in Santa Barbara.

--- SurfMap/CallSurflineAPI.swift
+++ SurfMap/CallSurflineAPI.swift
@@ -11,7 +11,24 @@ import Alamofire
 
 
 class SurfData {
+    let SB_spot_ids = ["Refugio": 4991, "El Capitan": 4993, "Sands": 4994, "Coal Oil Point": 4995,
+                       "Campus Point": 4997, "Leadbetter": 4990, "Sandspit": 4998,
+                       "Santa Barbara Harbor": 139341, "Hammond's": 4999,
+                       "Carpenteria State Beach": 5001, "Tarpits": 5000, "Rincon": 4197]
     var dicts: Array<NSDictionary> = []
+    var surf_max = [Int : [[Float]]]()
+    var surf_min = [Int : [[Float]]]()
+    var coordinates = [Int : (Float, Float)]()
+
+    init() {
+        // Find all JSON for the beaches in SB and place data into self.dicts
+        for id in SB_spot_ids.values {
+            get_surfline_data(spot_id: UInt32(id), surfdata: self)
+        }
+        self.surf_max = extract_Surf_data(dataKey: "surf_max", surfdata: self)
+        self.surf_min = extract_Surf_data(dataKey: "surf_min", surfdata: self)
+        self.coordinates = extract_lat_lon_data(surfdata: self)
+    }
 
     func add_data(data: NSDictionary) -> Void {
         self.dicts.append(data)
@@ -28,7 +45,14 @@ func get_surfline_data(spot_id: UInt32, surfdata: SurfData) -> Void {
 }
 
 
-func get_Surf_data(dataKey: String, surfdata: SurfData) -> [Int : [[Float]]] {
+func extract_Surf_data(dataKey: String, surfdata: SurfData) -> [Int : [[Float]]] {
+    /*
+     Return dictionary with Int as key and Array of Arrays containing Floats as values.
+
+     For surf_max as dataKey:
+        dict[id][0] is an Array containing surf height in feet for today at 4am, 10am, 4pm, and 10pm.
+        dict[id][1] is for the next day and so on.
+     */
     var Surf_data = [Int : [[Float]]]()
     for dict in surfdata.dicts {
         let id = (dict["id"] as! NSString).intValue
@@ -37,4 +61,18 @@ func get_Surf_data(dataKey: String, surfdata: SurfData) -> [Int : [[Float]]] {
         Surf_data[Int(id)] = data
     }
     return Surf_data
+}
+
+func extract_lat_lon_data(surfdata: SurfData) -> [Int : (Float, Float)] {
+    /*
+     Return dictionary with Int as keys and Tuple(latitude, longitude) as values.
+     */
+    var lat_lon_data = [Int : (Float, Float)]()
+    for dict in surfdata.dicts {
+        let id = (dict["id"] as! NSString).intValue
+        let lat = (dict["lat"] as! NSString).floatValue
+        let lon = (dict["lon"] as! NSString).floatValue
+        lat_lon_data[Int(id)] = (lat, lon)
+    }
+    return lat_lon_data
 }
